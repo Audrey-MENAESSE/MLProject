8813e01006e8e3aabdf3e68e0be013a319f25a93
Audrey MENAESSE <audrey.menaesse@epfl.ch>
2020-10-22 17:21:40 +0200

    Update implementations.py

--- implementations.py
+++ implementations.py
@@ -3,7 +3,98 @@ import numpy as np
     # ***************************************************
     #  Useful Functions
     # ***************************************************
+""" Feature Processing functions """
+# Replacing the missing values by the average 
+def replace_missing (tX):
+    for i in range(len(tX[0])):
+        average = np.mean(tX[tX[:,i]!=-999, i])
+        inds = np.where(tX[:,i]==-999)
+        tX[inds, i] = average
+    return tX
 
+def cat_features (tX, headers) :
+    # Finding the number of the column containing the categorical feature
+    col = np.where(headers=='PRI_jet_num')[0]
+    
+    # The possible entries of feature PRI_jet_num are 0, 1, 2, 3
+    inds0 = np.where(tX[:, col] == 0)[0]
+    inds1 = np.where(tX[:, col] == 1)[0]
+    inds2 = np.where(tX[:, col] == 2)[0]
+    inds3 = np.where(tX[:, col] == 3)[0]
+
+    # initialize new columns as zeros
+    pri_jet0 = np.zeros(tX.shape[0])
+    pri_jet1 = np.zeros(tX.shape[0])
+    pri_jet2 = np.zeros(tX.shape[0])
+    pri_jet3 = np.zeros(tX.shape[0])
+
+    # set ones to appropriate columns
+    pri_jet0[inds0] = 1
+    pri_jet1[inds1] = 1
+    pri_jet2[inds2] = 1
+    pri_jet3[inds3] = 1
+
+    # create a vector with the new categorical features
+    new_cat = np.column_stack((pri_jet0, pri_jet1, pri_jet2, pri_jet3))
+    
+    # delete original jet_num column
+    tX = np.delete(tX, col, axis=1)
+    headers = np.delete(headers, col)
+    
+    # append new columns
+    #tX = np.column_stack((tX,pri_jet0, pri_jet1, pri_jet2, pri_jet3))
+    new_heads = ['pri_jet_num0', 'pri_jet_num1', 'pri_jet_num2', 'pri_jet_num3']
+    #head_ = np.append(head_, new_heads)
+    
+    return tX, new_cat
+
+def standardize(x):
+    ''' Standardize each column of the input. '''
+    x = (x-np.mean(x, axis=0))/np.std(x, axis=0)
+    return x
+
+def PCA_bias (tX) :
+    m, n = tX.shape
+    dataset=tX
+    dataset -= tX.mean(axis=0)
+    R = np.cov(dataset, rowvar=False)
+    evals, evects = np.linalg.eigh(R)
+    # Sorting the eigenvalues in a decreasing order
+    index = np.argsort(evals)[::-1]
+    evals = evals[index]
+    evects = evects[:,index]
+    #evects = evects[:, :n_features]
+    data = np.dot(evects.T, dataset.T).T
+    
+    # Adding the bias term of the model
+    data_ones = np.c_[np.ones((len(tX), 1)), data]
+    
+    return (data_ones)
+
+def build_poly (tX, degree):
+    N= len(tX[0])
+    new_tX = np.ones(tX.shape[0])
+    for i in range(N):
+        feature = tX[:,i]
+        j= np.arange(1, degree+1)
+        feature=feature.reshape(-1,1)
+        ft = feature**(j)
+        new_tX = np.column_stack((new_tX, ft))
+    new_tX = np.delete(new_tX, 0, axis=1)
+    return new_tX
+
+def arrange_data (tX, headers, degree):
+    data = replace_missing (tX)
+    data, cat = cat_features (data, headers)    
+    data = build_poly(data, degree)
+    data = standardize (data)
+    data = np.c_[data, cat]
+    # data = PCA_bias(data)
+    return data
+
+
+
+""" Data splitting functions """
 
 def split_data(x, y, ratio, seed=1):
     """
@@ -135,3 +226,19 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
             print("Current iteration={i}, training loss={l}".format(i=iter, l=loss))
     
     return w, loss
+
+def calculate_accuracy (y_test, x_test, coeffs) :
+    y_pred = x_test.dot(coeffs)
+    N = len (y_test)
+    T = 0
+    F = 0
+    for i in range (N) :
+        if y_test[i]*y_pred[i]>0:
+            T+=1
+        else : 
+            F+=1
+    accuracy = 100*T/N
+    print("There are {acc} % of correct predictions".format(
+              acc = accuracy))
+    
+calculate_accuracy(y_test, x_test, coeffs)
